f4509bd8255b6a317d8b86491bf78cd555f67b27
SasikumarGunasekaran1998 <50619565+SasikumarGunasekaran1998@users.noreply.github.com>
2019-05-17 19:11:19 +0530

    Create Armsvar.py

---
+++ Armsvar.py
@@ -0,0 +1,24 @@
+n,k = input().split() #Getting the input
+n = int(n)
+k = int(k)
+for i in range (n,(k+1),1):
+   sum = 0
+   temp = i
+   check = i
+   use = i
+   while(temp>0):
+      use = use % 10
+      t = use**3
+      sum = sum + t
+      temp = temp//10
+      use = temp
+   if(sum == check):
+      print(sum)
+   
+      
+      
+      
+      
+   
+   
+   
