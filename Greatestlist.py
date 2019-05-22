8c48df0d0d302ff4586031acf7d573fcfc3cf710
SasikumarGunasekaran1998 <50619565+SasikumarGunasekaran1998@users.noreply.github.com>
2019-05-22 19:27:47 +0530

    Create Greatestlist.py

---
+++ Greatestlist.py
@@ -0,0 +1,23 @@
+n = int(input())
+num = input().split()
+l = len(num)
+if(l!= n):
+   print("Please enter the specfied value")
+else:
+   for i in range (n):
+         count = 0
+         for j in range(n):
+            if(int(num[i])>0):
+               if(num[i] > num[j]):
+                  count = count+1
+               if(count>=n-1):
+                  print(num[i])
+                  break
+            else:
+               if(num[i] < num[j]):
+                  count = count+1
+               if(count>=n-1):
+                  print(num[i])
+                  
+               
+       
