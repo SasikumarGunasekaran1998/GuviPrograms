e706d003f223e214b5ce05851957fea1dd10a024
SasikumarGunasekaran1998 <50619565+SasikumarGunasekaran1998@users.noreply.github.com>
2019-05-28 17:05:34 +0530

    Update maxoften.py

--- maxoften.py
+++ maxoften.py
@@ -6,7 +6,8 @@ for i in range(l):
     count = 0
     for j in range(l):
         inp[j] = int(inp[j])
-        if(inp[i]>inp[j]):
+        if(inp[i]>=inp[j]):
             count = count+1
-    if(count >= (l-1)):
+    if(count >= (l)):
         print(inp[i])
+        break
