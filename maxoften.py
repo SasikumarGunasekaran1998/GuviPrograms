5180cc98737d6855ffd8688915637817292d0ae9
SasikumarGunasekaran1998 <50619565+SasikumarGunasekaran1998@users.noreply.github.com>
2019-05-28 17:02:33 +0530

    Create maxoften.py

---
+++ maxoften.py
@@ -0,0 +1,12 @@
+inp = input().split()
+l = len(inp)
+count = 0
+for i in range(l):
+    inp[i] = int(inp[i])
+    count = 0
+    for j in range(l):
+        inp[j] = int(inp[j])
+        if(inp[i]>inp[j]):
+            count = count+1
+    if(count >= (l-1)):
+        print(inp[i])
